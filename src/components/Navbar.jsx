ef3c187df901d536c440db0a6e885bfa9f101fab
Efoneli <abbaefoneli@gmail.com>
2023-02-27 15:13:24 +0100

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutLined, MoneyCollectOutlined, BulbOutLined, FundOutLined, MenuOutlined } from 'antd-design/icons';
+
+const Navbar = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Navbar
