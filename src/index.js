70144f2d578431f8f94e3f0a0f596c48d354e5cd
Efoneli <abbaefoneli@gmail.com>
2023-02-27 22:54:46 +0100

    api with redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from './react-redux';
+import { Provider } from 'react-redux';
 
 import 'antd/dist/reset.css';
 import store from './app/store';
